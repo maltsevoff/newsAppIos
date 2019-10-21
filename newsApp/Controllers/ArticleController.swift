258f47d71050034d36328256fa1b2295f50d8812
maltsevoff <alexmalczev@gmail.com>
2019-10-21 11:16:57 +0300

    aritcleController with working tableView

--- newsApp/Controllers/ArticleController.swift
+++ newsApp/Controllers/ArticleController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 
-class ArticleController: UIViewController {
+class ArticleController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	@IBOutlet weak var articleDateLabel: UILabel!
 	@IBOutlet weak var articleTitleLabel: UILabel!
+	@IBOutlet weak var imagesTableView: UITableView!
 	
 	var articleNumber: Int?
-	var imagesUrl: [String] = []
+	var images: [String] = []
 	var thisArticle: News?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		if articleNumber != nil {
-			self.imagesUrl = imagesForArticle(index: articleNumber!)
-			print(imagesUrl)
+			self.images = imagesForArticle(index: articleNumber!)
+			print(images)
 			self.thisArticle = articles[articleNumber!]
 			setInfo()
 		}
@@ -33,4 +34,18 @@ class ArticleController: UIViewController {
 		articleTitleLabel.text = self.thisArticle?.title
 		articleTitleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
 	}
+	
+	//MARK: - Table View
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return images.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
+		
+		cell.imageUrl = images[indexPath.row]
+		return cell
+	}
+	
 }
